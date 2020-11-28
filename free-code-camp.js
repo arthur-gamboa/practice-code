fdc42743be9ca7aba19b83a3d348d53dfe6ec2cb
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-11-28 02:10:36 -0600

    continuing freecodecamp practice

--- free-code-camp.js
+++ free-code-camp.js
@@ -92,3 +92,32 @@ function timesFive(input) {
 }
 //*****************************************//
 //43.//
+// Setup
+var sum = 0;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+//*****************************************//
+//44.//
+// Setup
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+//*****************************************//
+//45.//
