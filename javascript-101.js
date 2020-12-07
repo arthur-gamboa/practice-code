b393266cce9afb96664865107384b6fed5209bcb
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-12-06 20:18:36 -0600

    continuing 101

--- javascript-101.js
+++ javascript-101.js
@@ -778,3 +778,38 @@ assert(second("ubuntu"), "b", "Exercise 51");
 assert(second([1, 2, 3]), 2, "Exercise 51");
 assert(second(["JS", "is", "awesome"]), "is", "Exercise 51");
 addToDone("Exercise 51 is correct.")
+
+
+// Exercise 52
+// Write a function definition named third that takes in sequence and returns the third value of that sequence.
+function third(str) {
+    return str[2];
+}
+assert(third("ubuntu"), "u", "Exercise 52");
+assert(third([1, 2, 3]), 3, "Exercise 52");
+assert(third(["JS", "is", "awesome"]), "awesome", "Exercise 52");
+addToDone("Exercise 52 is correct.")
+
+
+// Exercise 53
+// Write a function definition named forth that takes in sequence and returns the forth value of that sequence.
+function forth(str) {
+    return str[3];
+}
+assert(forth("ubuntu"), "n", "Exercise 53");
+assert(forth([1, 2, 3, 4]), 4, "Exercise 53");
+assert(forth(["JS", "is", "awesome", "right?"]), "right?", "Exercise 53");
+addToDone("Exercise 53 is correct.")
+
+
+// Exercise 54
+// Write a function definition named last that takes in sequence and returns the last value of that sequence.
+function last(str) {
+    var lastSeq = str[str.length -1];
+    return lastSeq;
+}
+assert(last("ubuntu"), "u", "Exercise 54");
+assert(last([1, 2, 3, 4]), 4, "Exercise 54");
+assert(last(["JS", "is", "awesome"]), "awesome", "Exercise 54");
+assert(last(["kiwi", "mango", "guava"]), "guava", "Exercise 54");
+addToDone("Exercise 54 is correct.")
