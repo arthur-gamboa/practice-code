50e988ae11233471585a2556efb1ff18c73f1b57
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-12-13 20:52:10 -0600

    continuing bootstrapDOM practice

--- pc_js/bootstrap-practice-DOM.js
+++ pc_js/bootstrap-practice-DOM.js
@@ -1,5 +1,36 @@
-(function call() {
-    var exElements = document.getElementById("choose-roast");
+// var titles = document.getElementsByClassName("row");
+//
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+//
+// Array.from(titles).forEach(function(item) {
+//     console.log(item)
+// })
 
-    console.log(exElements);
-})
+//*********************************************************//
+
+// var exElement = document.getElementsByClassName("row");
+//
+// console.log(exElement[0]);
+//
+// for (var i = 0; i < exElement.length; i++) {
+//     console.log(exElement[i]);
+// }
+//
+// var mainHeading = document.getElementById("main-heading")
+//
+// console.log(mainHeading);
+
+//*********************************************************//
+
+//examine the document object //
+// console.dir(document);
+
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+// document.title = 123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
