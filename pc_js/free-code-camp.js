968953f3e66715401223c5598514a118124b19b2
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2020-12-28 17:00:11 -0600

    continuing freecodecamp js

--- pc_js/free-code-camp.js
+++ pc_js/free-code-camp.js
@@ -552,16 +552,36 @@ function cc(card) {
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
+//******************************************************************************//
+// DIFFERENT METHOD FOR 70. //
+var count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    }
+    if (count > 0) {
+        return count + " Bet";
+    } else {
+        return count + " Hold";
+    }
+    // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
 //*****************************************//
 //71.//
-var myDog = {
-“name”: “Joe”,
-“legs”: 4,
-“tails”: 1,
-“friends”:[“many”, “Ade”]
-
-};
-
-
-//*****************************************//
-//72.//
